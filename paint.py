f54b1159207c9b857fbb3d4c16826e9f446cf989
Erick Potts <a01751118@itesm.mx>
2021-05-06 20:54:41 -0500

    Ultima version

--- paint.py
+++ paint.py
@@ -9,10 +9,15 @@ Exercises
 5. Add width parameter.
 
 """
+# Codigo modificado por:
+# Autor: Erick Potts Berruga
+# Autor: Jose Gabriel Arana Jacuinde
 
-
+"""Se importan las librerias que se van a usar en el codigo"""
+import turtle          #Para utilizar las funciones turtle
 from turtle import *
 from freegames import vector
+
 
 def line(start, end):
     "Draw line from start to end."
@@ -35,17 +40,47 @@ def square(start, end):
 
     end_fill()
 
+
 def circle(start, end):
     "Draw circle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    turtle.circle((end.x-start.x)/2)    
+    end_fill()
+    
 
 def rectangle(start, end):
     "Draw rectangle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
 
+    for count in range(2):
+        forward(end.x - start.x)
+        left(90)
+        forward(end.y - start.y)
+        left(90)
+
+    end_fill()
+    
+    
 def triangle(start, end):
     "Draw triangle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    
+    for count in range(2):
+        forward(end.x - start.x)
+        left(120)
+    for count in range(2):
+        left(120)
+    end_fill()
+    
 
 def tap(x, y):
     "Store starting point or draw shape."
@@ -66,16 +101,23 @@ def store(key, value):
 state = {'start': None, 'shape': line}
 setup(420, 420, 370, 0)
 onscreenclick(tap)
+
 listen()
 onkey(undo, 'u')
+
+# Define las teclas que cambian de color
 onkey(lambda: color('black'), 'K')
 onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+onkey(lambda: color('orange'), 'O')
+
+# Define las tecladas asociadas a cada figura
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
 onkey(lambda: store('shape', rectangle), 'r')
 onkey(lambda: store('shape', triangle), 't')
+
 done()
